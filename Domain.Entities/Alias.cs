78204e5bacf199d3b745e75c6ac3e7c43a5ddf6d
memeza <nkosinol@yahoo.com>
2016-11-11 08:51:13 +0200

    Changes on the repositories

---
+++ Domain.Entities/Alias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Alias
+    {
+
+        public Guid Alias_Id { get; set; }
+        public Guid Artist_Id { get; set; }
+        public string Name { get; set; }
+        public virtual Artist Artist { get; set; }
+    }
+}
--- Domain.Entities/Artist.cs
+++ Domain.Entities/Artist.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domain.Entities
 {
@@ -6,7 +7,7 @@ namespace Domain.Entities
     {
         public Guid Artist_Id { get; set; }
         public string Name { get; set; }
-        public string Aliases { get; set; }
+        public virtual ICollection<Alias> Aliases { get; set; }
         public string Country { get; set; }
     }
 }
